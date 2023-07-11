83303cc33b06be04ecc432e8157f387c65a8dc30
TanpreetJolly <tanpreetjolly@pop-os.localdomain>
2023-07-11 12:24:19 +0530

    backend testing 3

--- server/server.js
+++ server/server.js
@@ -14,12 +14,11 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('../client/dist'));
+app.use(express.static("../client/dist"));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/dist/', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
 });
-
 
 app.post("/pay", async (req, res) => {
   console.log(req.body.token);
