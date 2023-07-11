113a0e05531ba4721d753ad81f2be8c5f1c0fbcf
TanpreetJolly <tanpreetjolly@pop-os.localdomain>
2023-07-11 12:29:03 +0530

    npm configuration

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const port = process.env.PORT;
